2ca468bb6e318ef84b33b3b40ae32878ad610bc6
Shaiko-Vitaliy <shaiko2586@gmail.com>
2023-03-29 19:19:31 +0300

    create wed app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,28 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "5000");
+        return Integer.parseInt(port);
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+           config.enableDevLogging();
+        });
+        addRoutes(app);
+        return app;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Javalin app = getApp();
+        app.start(getPort());
     }
 }
